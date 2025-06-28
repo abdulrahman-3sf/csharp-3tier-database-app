a0f8874bd57886b04f9cb6ac0e287d636ea78adf
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-06-28 11:31:14 +0300

    Add project files

---
+++ ContactsBusinessLayer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using ContactsDataAccessLayer;
+
+
+namespace ContactsBusinessLayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ContactsConsoleApp-PresentationLayer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using ContactsBusinessLayer;
+
+namespace ContactsConsoleApp_PresentationLayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
